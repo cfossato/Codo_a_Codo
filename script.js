da87a6b74a2f7914a9c50629bf0edbf9b8bd249d
Camila Fossato <camilafossato@hotmail.com>
2023-06-12 18:41:42 -0300

    js_y_html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-window.addEventListener('DOMContentLoaded', function() {
+/*window.addEventListener('DOMContentLoaded', function() {
     var cantidadInput = document.getElementById('exampleInputCantidad');
     var categoriaSelect = document.getElementById('exampleInputCategoria');
     var totalAPagarInput = document.getElementById('exampleInputTotalapagar');
@@ -40,3 +40,37 @@ window.addEventListener('DOMContentLoaded', function() {
       totalAPagarInput.value = '$' + totalAPagar.toFixed(2);
     });
   });
+*/
+
+ function calcularTotal(cantidad, categoria) {
+    let precioEntrada = 200;
+    let estudiante = 0.2;
+    let trainee = 0.5;
+    let junior = 0.85;
+    let total = 0;
+
+    if (categoria === "estudiante") {
+        total = cantidad * estudiante * precioEntrada;
+    } else if (categoria === "trainee") {
+        total = cantidad * trainee * precioEntrada;
+    } else {
+        total = cantidad * junior * precioEntrada;
+    }
+
+    return total;
+}
+
+let formulario = document.getElementById("form");
+let cantidad = document.getElementById("cantidad");
+let categoria = document.getElementById("categoria");
+let divResultado = document.querySelector("resultado");
+
+formulario.addEventListener("reset", () => {
+    divResultado.textContent = "Total a pagar $";
+});
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    divResultado.textContent = "Total a pagar $ " + calcularTotal(cantidad.value, categoria.value);
+}); 
+
